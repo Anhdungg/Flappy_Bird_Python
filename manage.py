8e62fd7986fe7a2e1d20a63835f93fcfd71107b4
Anhdungg <ledung.ht17@gmail.com>
2021-03-31 19:29:16 +0700

    Begin animated

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@ import pygame
 
 SIZE = WIDTH, HEIGHT = 500, 600
 FPS = 30
-ICON = pygame.image.load("bird.png")
+ICON = pygame.image.load("sprites/bird.png")
 BACKGROUND_MAIN = (32, 176, 186)
 CLOCK = pygame.time.Clock()
+
+X = 50
+Y = 400
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
@@ -16,7 +19,7 @@ while(statusRun):
     screen.fill(BACKGROUND_MAIN)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            statusRun = False;
+            statusRun = False
 
     pygame.display.flip()
     CLOCK.tick(FPS)
