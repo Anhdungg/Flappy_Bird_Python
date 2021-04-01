64b284de0e750f0b3b574941898ffe9db8381836
anh dung <ledung.ht17@gmail.com>
2021-04-01 23:03:40 +0700

    Update animated bird and floor

--- manage.py
+++ manage.py
@@ -1,26 +1,95 @@
 import pygame
+import time
+from pynput import keyboard
 
-SIZE = WIDTH, HEIGHT = 500, 600
-FPS = 30
+SIZE = WIDTH, HEIGHT = 288, 512
+FPS = 60
+POS_Y_BIRD_UP = 150
 ICON = pygame.image.load("sprites/bird.png")
-BACKGROUND_MAIN = (32, 176, 186)
+ICON.convert
+BACKGROUND_MAIN = pygame.image.load("sprites/background-day.png")
+BACKGROUND_MAIN.convert
+BACKGROUND_BEGIN = pygame.image.load("sprites/message.png")
+BACKGROUND_BEGIN.convert
+BIRTH_RED = [pygame.image.load("sprites/bluebird-upflap.png"), pygame.image.load("sprites/bluebird-midflap.png"), 
+pygame.image.load("sprites/bluebird-downflap.png")]
+FLOOR_IMAGE = pygame.image.load("sprites/base.png")
+FLOOR_IMAGE.convert
 CLOCK = pygame.time.Clock()
 
-X = 50
-Y = 400
+X = 30
+Y = 50
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_icon(ICON)
 pygame.display.set_caption("Flappy Bird")
 
-statusRun = True
-while(statusRun):
-    screen.fill(BACKGROUND_MAIN)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            statusRun = False
+def begin():
+    statusRun = True
+    rect = BACKGROUND_BEGIN.get_rect()
+    rect.center = WIDTH//2, HEIGHT//2
+    while statusRun:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return True
+        screen.blit(BACKGROUND_BEGIN, rect)
+        pygame.display.update()
+        CLOCK.tick(FPS)
 
-    pygame.display.flip()
-    CLOCK.tick(FPS)
-pygame.quit()
+def main():
+    if not begin():
+        pygame.quit()
+        return
+    statusRun = True
+    statusKeyDown = False
+    countImageBird = 0
+    posYBird = 50
+    countPosXFloor = 0
+    count=2
+    while(statusRun):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                statusRun = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Y = posYBird - POS_Y_BIRD_UP
+                    statusKeyDown = True
+                    count = 10 
+                if event.key == pygame.K_ESCAPE:
+                    statusRun = False
+        if statusKeyDown :
+            if posYBird>Y:
+                posYBird = posYBird - count
+                if posYBird < 0:
+                    posYBird = 0
+                    statusKeyDown = False
+                    count = 2
+                count = abs(count-0.32)
+            else:
+                statusKeyDown = False
+                count = 2 
+        else:
+            if posYBird<375:
+                posYBird+=count
+                count+=0.5
+            else:
+                count = 10
+
+        countPosXFloor-=1
+        countImageBird+=1
+        if countImageBird>17:
+            countImageBird=0
+        if countPosXFloor < -48:
+            countPosXFloor = 0
+        screen.blit(BACKGROUND_MAIN, (0,0))
+        screen.blit(BIRTH_RED[int(countImageBird/6)], (X,posYBird))
+        screen.blit(FLOOR_IMAGE, (countPosXFloor, 400))
+        pygame.display.update()
+        CLOCK.tick(FPS)
+    pygame.quit()
+
+main()
